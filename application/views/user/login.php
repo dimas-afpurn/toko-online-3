fa1897b9f5fb552e6fa9fc5b4d4cebdd7d7079d8
dimas-afpurn <dimasafpurn@gmail.com>
2021-11-03 11:59:42 +0700

    view login

--- application/views/user/login.php
+++ application/views/user/login.php
@@ -2,70 +2,71 @@
 <html lang="en">
 
 <head>
-    
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login | blonjosam.com</title>
-    <link rel="stylesheet" href="<?= base_url() ?>assets/css/font-awesome.min.css" />
-    <link href='https://fonts.googleapis.com/css?family=Aladin' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Montserrat|Raleway:400,200,300,500,600,700,800,900,100' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900' rel='stylesheet' type='text/css'>
-    
-    <link href="<?= base_url() ?>assets/css/auth.scss" rel="stylesheet" type="text/css" media="all" />
+
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login | blonjosam.com</title>
+	<link rel="stylesheet" href="<?= base_url() ?>assets/css/font-awesome.min.css" />
+	<link href='https://fonts.googleapis.com/css?family=Aladin' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Montserrat|Raleway:400,200,300,500,600,700,800,900,100' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Playfair+Display:400,700,900' rel='stylesheet' type='text/css'>
+
+	<link href="<?= base_url() ?>assets/css/auth.scss" rel="stylesheet" type="text/css" media="all" />
 
 
 </head>
 
 <body>
-<div class="container" id="container">
-	<div class="form-container sign-up-container">
-		<form action="<?php echo base_url('insert_register')?>" method="post">
-			<h1>Create Account</h1>
-			
-			<span>or use your email for registration</span>
-			<input type="text" placeholder="Name"  name="nama_member"/>
-			<input type="email" placeholder="Email" name="email"/>
-			<input type="password" placeholder="Password" name="password"/>
-			<button>Sign Up</button>
-		</form>
-	</div>
-	<div class="form-container sign-in-container">
-		<form method="POST" action="<?php echo base_url("user/home/check_login"); ?>">
-			<h1>Sign in</h1>
-			<span>or use your account</span>
-			<input type="email" placeholder="Email" name="email"/>
-			<input type="password" placeholder="Password" name="password"/>
-			<button>Sign In</button>
-		</form>
-	</div>
-	<div class="overlay-container">
-		<div class="overlay">
-			<div class="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" id="signIn">Sign In</button>
-			</div>
-			<div class="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
+	<div class="container" id="container">
+		<div class="form-container sign-up-container">
+			<form action="<?php echo base_url('insert_register') ?>" method="post">
+				<h1>Registrasi Member</h1>
+
+				<span>Belanja lebih nyaman sebagai member.</span>
+				<input type="text" placeholder="Nama" name="nama_member" required oninvalid="this.setCustomValidity('nama masih kosong')" oninput="setCustomValidity('')" />
+				<input type="email" placeholder="Email" name="email" required oninvalid="this.setCustomValidity('email masih kosong atau belum sesuai')" oninput="setCustomValidity('')" />
+				<input type="password" placeholder="Password" name="password" required oninvalid="this.setCustomValidity('password masih kosong')" oninput="setCustomValidity('')" />
+				<button>Daftar</button>
+			</form>
+		</div>
+		<div class="form-container sign-in-container">
+			<form method="POST" action="<?php echo base_url("user/home/check_login"); ?>">
+				<h1>Login</h1>
+				<span>masuk ke akun member Anda</span>
+				<input type="email" placeholder="Email" name="email" required oninvalid="this.setCustomValidity('email masih kosong atau belum sesuai')" oninput="setCustomValidity('')" />
+				<input type="password" placeholder="Password" name="password" required oninvalid="this.setCustomValidity('password masih kosong')" oninput="setCustomValidity('')" />
+				<button>Log In</button>
+			</form>
+		</div>
+		<div class="overlay-container">
+			<div class="overlay">
+				<div class="overlay-panel overlay-left">
+					<h1>Welcome Back Sam!</h1>
+					<p>sudah punya akun member?</p>
+					<button class="ghost" id="signIn">Log In</button>
+				</div>
+				<div class="overlay-panel overlay-right">
+					<h1>Hello, Sam!</h1>
+					<p>Belum terdaftar member? </p>
+					<button class="ghost" id="signUp">Daftar</button>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </body>
 
 <script>
-  const signUpButton = document.getElementById('signUp');
-    const signInButton = document.getElementById('signIn');
-    const container = document.getElementById('container');
+	const signUpButton = document.getElementById('signUp');
+	const signInButton = document.getElementById('signIn');
+	const container = document.getElementById('container');
 
-    signUpButton.addEventListener('click', () => {
-        container.classList.add("right-panel-active");
-    });
+	signUpButton.addEventListener('click', () => {
+		container.classList.add("right-panel-active");
+	});
 
-    signInButton.addEventListener('click', () => {
-        container.classList.remove("right-panel-active");
-    });
-</script>    
+	signInButton.addEventListener('click', () => {
+		container.classList.remove("right-panel-active");
+	});
+</script>
+
 </html>
